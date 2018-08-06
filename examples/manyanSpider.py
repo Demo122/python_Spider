cbdd455e7a7a20ece175dcfa2908a69059d419db
Demo122 <1094346704@qq.com>
2018-08-07 01:01:54 +0800

    爬取猫眼top100电影榜单

--- examples/manyanSpider.py
+++ examples/manyanSpider.py
@@ -4,3 +4,54 @@
 @author:DQ
 @time:2018/8/6 21:31
 """
+import requests
+import re
+import json
+
+
+# 抓取首页
+def get_one_page(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/68.0.3440.84 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    return None
+
+
+# 正则提取
+def parse_one_page(html):
+    pattern = re.compile('<dd.*?board-index.*?(\d+).*?data-src="(.*?)".*?name.*?a.*?>(.*?)</a>.*?star.*?>\s+(.*?)\s+</p>'
+                         '.*?releasetime">(.*?)</p>.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    titles = re.findall(pattern, html)
+    for item in titles:
+        yield {
+            'index':item[0],
+            'image':item[1],
+            'title':item[2],
+            'actor':item[3],
+            'releasetime':item[4],
+            'score':item[5]+item[6]
+        }
+
+#写入文件
+def write_to_file(content):
+    with open('maoyan_movies_top1001.txt','a',encoding='utf-8') as f:
+        #print(type(json.dumps(content)))
+        f.write(json.dumps(content,ensure_ascii=False)+'\n')
+
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset='+str(offset)
+    html = get_one_page(url)
+    # for i in parse_one_page(html):
+    #     print(type(j))
+    for content in parse_one_page(html):
+        write_to_file(content)
+
+if __name__ == '__main__':
+    #分页爬取，修改offset的偏移量
+    for i in range(10):
+        main(i*10)
