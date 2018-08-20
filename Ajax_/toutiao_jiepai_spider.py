ecc1c486d131d472fc3453fbcc8cd210f4e780c5
Demo122 <1094346704@qq.com>
2018-08-21 01:14:54 +0800

    爬取今日头条街拍美图(改进版)(爬取高清图，多进程)

--- Ajax_/toutiao_jiepai_spider.py
+++ Ajax_/toutiao_jiepai_spider.py
@@ -47,30 +47,36 @@ def get_image_url(json):
 
 
 def save_images(item):
-    base_path='./街拍美图/'+item.get('title')
+    base_path = './街拍美图/' + item.get('title')
     if not os.path.exists('./街拍美图'):
         os.mkdir('./街拍美图')
     if not os.path.exists(base_path):
         os.mkdir(base_path)  # 创建图片文件夹
     try:
-        #查看了网页，将爬取的图片链接中的'list'替换为'origin'以下载高清美图
+        # 查看了网页，将爬取的图片链接中的'list'替换为'origin'以下载高清美图
         image_origin_url = 'https:' + item.get('image_url').replace('list', 'origin')
-        response = requests.get(image_origin_url)
+        response = requests.get(image_origin_url, timeout=2)
         if response.status_code == 200:
             file_path = '{0}/{1}.{2}'.format(base_path, md5(response.content).hexdigest(), 'jpg')
             if not os.path.exists(file_path):
                 with open(file_path, 'wb') as f:
                     f.write(response.content)
+                    print('Download:', file_path)
+                    return True
             else:
-                print('Already Download to ', file_path)
-    except requests.ConnectionError:
+                print('Already Download ', file_path)
+    except (requests.ConnectionError, requests.ConnectTimeout) as e:
+        print(e.args)
         print('Failed to save images!')
+        return False
 
 
 def main(offset):
+    number = 0
     json = get_page(offset)
     for item in get_image_url(json):
-        save_images(item)
+        if save_images(item):
+            number += 1
 
 
 GROUP_START = 1
@@ -78,13 +84,14 @@ GROUP_END = 20
 
 if __name__ == '__main__':
     time_start = time.time()
-    pool = Pool()
+    pool = Pool(processes=4)
     groups = ([x * 20 for x in range(GROUP_START, GROUP_END + 1)])
-    pool.map(main, groups)
+    number = (pool.map(main, groups))  # 获取所有进程结束后返回的结果
     pool.close()
     pool.join()
 
     # for offset in range(0,400,20):
     #     main(offset)
-    time_end=time.time()
-    print('共耗时：',time_end-time_start)
+    time_end = time.time()
+    print('\n共下载美图%d部'%number[0])
+    print('\n共耗时:{}s'.format(time_end - time_start))
